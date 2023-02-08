05e43e756afe6c7f664f33f8ae033ba0be8ef808
sanjay <sanjaykushites@gmail.com>
2023-02-08 10:48:59 +0530

    application properties settings

--- Snakeladder/src/main/java/com/snake/service/PlayerService.java
+++ Snakeladder/src/main/java/com/snake/service/PlayerService.java
@@ -3,10 +3,12 @@ package com.snake.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.snake.model.Player;
 import com.snake.repo.Playerdao;
 
+@Component
 public interface PlayerService {
 	
 	public Player getPlayer(int id);
--- Snakeladder/src/main/java/com/snake/service/PlayerServiceimpl.java
+++ Snakeladder/src/main/java/com/snake/service/PlayerServiceimpl.java
@@ -5,10 +5,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.snake.model.Player;
 import com.snake.repo.Playerdao;
 
+@Service
 public class PlayerServiceimpl implements PlayerService{
 
 	@Autowired
