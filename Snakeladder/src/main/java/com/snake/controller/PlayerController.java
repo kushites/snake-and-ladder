23ea56b2b1a7dffcb648a2aeab56e758c054f069
sanjay <sanjaykushites@gmail.com>
2023-02-08 10:35:22 +0530

    Controller

--- Snakeladder/src/main/java/com/snake/controller/PlayerController.java
+++ Snakeladder/src/main/java/com/snake/controller/PlayerController.java
@@ -1,11 +1,25 @@
 package com.snake.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.snake.model.Player;
+import com.snake.service.PlayerService;
 
 @RestController
 public class PlayerController {
 	
-	public List<Player>savePlayers(){
+	@Autowired
+	private PlayerService playerservice;
+	
+	@GetMapping("/saveplayer/{no}")
+	public List<Player>savePlayers(@PathVariable Integer no){
+		return playerservice.savePlayers(no);
 		
 	}
 
